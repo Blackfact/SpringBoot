71862d29699b7def87d90a3317ae3c220f126f11
wuzw <631899689@qq.com>
2021-02-09 15:03:28 +0800

    提交

--- spring-mybatis-test/src/main/java/com/owen/springmybatistest/utils/MyMapper.java
+++ spring-mybatis-test/src/main/java/com/owen/springmybatistest/utils/MyMapper.java
@@ -1,4 +1,16 @@
 package com.owen.springmybatistest.utils;
 
-public class MyMapper {
+import tk.mybatis.mapper.common.Mapper;
+import tk.mybatis.mapper.common.MySqlMapper;
+
+/**
+ * 自己的 Mapper
+ *  * 特别注意，该接口不能被扫描到，否则会出错
+ *  * <p>Title: MyMapper</p>
+ *  * <p>Description: </p>
+ *
+ */
+public interface MyMapper<T> extends Mapper<T>, MySqlMapper<T> {
 }
+
+
