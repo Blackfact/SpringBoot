886e86970827fc849dad820cb90069394d978964
wuzw <631899689@qq.com>
2021-02-09 11:04:46 +0800

    SpringBoot整合Mybatis

---
+++ spring-mybatis-test/src/main/java/com/owen/springmybatistest/SpringMybatisTestApplication.java
@@ -0,0 +1,13 @@
+package com.owen.springmybatistest;
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMybatisTestApplication.class, args);
+    }
+
+}
---
+++ spring-mybatis-test/src/test/java/com/owen/springmybatistest/SpringMybatisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.owen.springmybatistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
